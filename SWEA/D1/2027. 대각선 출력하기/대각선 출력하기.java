7ca58bd50560360a2744eca085ba205619e109ed
dnjstjt1297 <128368235+dnjstjt1297@users.noreply.github.com>
2024-05-07 15:49:53 +0900

    [D1] Title: 대각선 출력하기, Time: 177 ms, Memory: 22,388 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.*;
+
+class Solution {
+	
+	static boolean[] is_prime;
+	
+	public static void main(String args[]) throws Exception {
+		System.out.println("#++++");
+		System.out.println("+#+++");
+		System.out.println("++#++");
+		System.out.println("+++#+");
+		System.out.println("++++#");
+	}
+}
+
