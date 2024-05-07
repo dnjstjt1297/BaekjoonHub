d6bef18c2b7f546d97382fdc8558caf2af29a6d2
dnjstjt1297 <128368235+dnjstjt1297@users.noreply.github.com>
2024-05-07 16:02:35 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 142 ms, Memory: 25,468 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/README.md
@@ -0,0 +1,15 @@
+# [D1] 거꾸로 출력해 보아요 - 1545 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV2gbY0qAAQBBAS0) 
+
+### 성능 요약
+
+메모리: 25,468 KB, 시간: 142 ms, 코드길이: 314 Bytes
+
+### 제출 일자
+
+2024-05-07 16:02
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.*;
+
+class Solution {
+	
+	static boolean[] is_prime;
+	
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=n;i>=0;i--) {
+			System.out.print(i+" ");
+		}
+	}
+}
+
