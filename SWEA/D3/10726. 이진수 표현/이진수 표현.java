9aefb59b572591a2035f49374dfc96d359985e65
dnjstjt1297 <128368235+dnjstjt1297@users.noreply.github.com>
2024-05-17 03:31:58 +0900

    [D3] Title: 이진수 표현, Time: 309 ms, Memory: 60,880 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -1,7 +1,4 @@
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 import java.io.FileInputStream;
 
@@ -27,29 +24,14 @@ class Solution {
 			int M = sc.nextInt();
 			String result = "";
 			int length = 0;
-			while(M>0) {
-				if(M%2==0) {
-					result = "0"+result;
-				}
-				else {
-					result = "1"+result;
-				}
-				M = M/2;
-				length++;
+			int tmp = 1;
+			tmp=tmp<<N;
+			tmp-=1;
+			boolean is_answer = false;
+			if((tmp&M)==tmp) {
+				is_answer = true;
 			}
-			boolean is_answer = true;
 			
-			for(int i= length-1;i>length-1-N;i--) {
-				if(i<0) {
-					is_answer = false;
-					break;
-				}
-				if(result.charAt(i)=='0') {
-					is_answer = false;
-					break;
-				}
-				
-			}
 			if(is_answer)
 				System.out.println("#"+test_case+" ON");
 			else
