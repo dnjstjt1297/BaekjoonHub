aee196dbea2fa1272f394847ef55a09b997e656b
dnjstjt1297 <128368235+dnjstjt1297@users.noreply.github.com>
2024-05-17 05:34:04 +0900

    [D3] Title: 카드 게임, Time: 132 ms, Memory: 27,176 KB -BaekjoonHub

---
+++ SWEA/D3/9778. 카드 게임/README.md
@@ -0,0 +1,15 @@
+# [D3] 카드 게임 - 9778 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXE0YJiK3QcDFAVX) 
+
+### 성능 요약
+
+메모리: 27,176 KB, 시간: 132 ms, 코드길이: 878 Bytes
+
+### 제출 일자
+
+2024-05-17 05:34
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/9778. 카드 게임/카드 게임.java
@@ -0,0 +1,51 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution {
+	public static void main(String args[]) throws Exception	{
+		
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int N =sc.nextInt();
+			int[] cards = new int[12];
+			for(int i=2;i<=9;i++) {
+				cards[i] = 4;
+			}
+			cards[11] = 4;
+			cards[10] = 16;
+			
+			int score = 0;
+			for(int i=0;i<N;i++) {
+				int c = sc.nextInt();
+				score+=c;
+				cards[c] --;
+			}
+			int numA = 0;
+			int numB = 0;
+			for(int i=2;i<=11;i++) {
+				if(score+i>21) {
+					numA+=cards[i];
+				}
+				else {
+					numB+=cards[i];
+				}
+			}
+			if(numA>=numB) {
+				System.out.println("#"+test_case+" STOP");
+			}
+			else {
+				System.out.println("#"+test_case+" GAZUA");
+			}
+			
+			
+		}
+	}
+	
+	
+}
