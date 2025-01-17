32761bf36d0b7b65d2d9746346ab2ee300a5fd7e
dnjstjt1297 <128368235+dnjstjt1297@users.noreply.github.com>
2025-01-18 00:52:13 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 568 ms, Memory: 48972 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,84 +1,76 @@
 import java.util.*;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.math.MathContext;
+import java.io.*;
 
-
-class Edge implements Comparable<Edge> {
-	private int v,w;
-	private int cost;
-	public Edge(int v, int w, int cost) {
-		this.v = v;
-		this.w = w;
-		this.cost = cost;
-	}
-	public int getV() {
-		return this.v;
-	}
-	public int getW(){
-		return this.w;
-	}
-	public int getCost(){
-		return this.cost;
-	}
-	@Override
-	public int compareTo(Edge n) {
-		return this.cost - n.cost;
-	}
-} 
+class Edge{
+    int v;
+    int w;
+    int cost;
+    public Edge(int v, int w, int cost){
+        this.v = v;
+        this.w = w;
+        this.cost = cost;
+    }
+}
 
 public class Main {
-	static int V,E;
-	static ArrayList<Edge> edges;
-	static int[] parent;
-	static int res =0;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		
-		edges = new ArrayList<Edge>();
-		
-		
-		for(int i=0;i<E;i++) {
-			st = new StringTokenizer(br.readLine());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			edges.add(new Edge(v,w,cost));
-		}
-		
-		parent = new int[V+1];
-		for(int i=0;i<=V;i++) parent[i] = i;
-		
-		kruskal();
-		System.out.println(res);
-	}
-	public static void kruskal() {
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		for(Edge e: edges) {
-			pq.add(e);
-		}
-		while(!pq.isEmpty()) {
-			Edge e = pq.poll();
-			if(!union(e.getV(),e.getW())) {
-				res += e.getCost();
-			}
-		}
-	}
-	public static int find(int x) {
-		if(parent[x] == x) {
-			return x;
-		}
-		return find(parent[x]);
-	}
-	public static boolean union(int x, int y) {
-		x = find(x);
-		y = find(y);
-		if(x == y) return true;
-		parent[y] = x;
-		return false;
-	}
+    static int[] parent;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        parent = new int[N+1];
+        for(int i=0;i<=N;i++) parent[i] = i;
+
+        Edge[] edges = new Edge[M];
+        for(int i =0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            edges[i] = new Edge(v,w,cost);
+        }
+        System.out.println(kruskal(edges,N,M));
+    }
+
+    public static int kruskal(Edge[] edges, int N, int M){
+        int result = 0;
+
+        Arrays.sort(edges,new Comparator<Edge>() {
+            @Override
+            public int compare(Edge e1, Edge e2) {
+                return e1.cost - e2.cost;
+            }
+        });
+
+
+        int cnt = 0;
+        for(int i =0;i<M;i++){
+            int pv = find(edges[i].v);
+            int pw = find(edges[i].w);
+
+            if(pv!=pw){
+                result+= edges[i].cost;
+                union(pv,pw);
+                cnt++;
+            }
+            if(cnt == N-1) break;
+        }
+        return result;
+    }
+
+    public static void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa < pb) parent[pb] = pa;
+        else parent[pa] = pb;
+    }
+
+    public static int find(int n){
+        if(parent[n] == n) return n;
+        parent[n] = find(parent[n]);
+        return parent[n];
+    }
 }
