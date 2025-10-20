65822814243cb40ef6d18d195c87b70c3e7b9023
dnjstjt1297 <128368235+dnjstjt1297@users.noreply.github.com>
2025-10-20 11:13:38 +0900

    [level 3] Title: 가장 먼 노드, Time: 31.19 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,46 +1,39 @@
 import java.util.*;
 
 class Solution {
+
+    ArrayList<Integer>[] graph;
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        int N = 0;
-        for(int[] e : edge){
-            N = Math.max(Math.max(e[0],e[1]), N);
-        }
-        
-        ArrayList<Integer>[] graph = new ArrayList[N+1];
-        for(int i = 1; i<N+1; i++){
+        graph = new ArrayList[n+1];
+        for(int i = 1; i<=n; i++){
             graph[i] = new ArrayList<>();
         }
         
-        for(int i = 0;i<edge.length;i++){
+        for(int i = 0; i< edge.length; i++){
             graph[edge[i][0]].add(edge[i][1]);
             graph[edge[i][1]].add(edge[i][0]);
         }
         
-        int[] inDegree = new int[N+1];
-        Arrays.fill(inDegree, N);
-        
-        inDegree[1] = 0;
+        boolean[] visited = new boolean[n+1];
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{1,0});
+        visited[1] = true;
         
-        
-        int max = 0;
+        int[] dist = new int[n+1];
         while(!q.isEmpty()){
             int[] cur = q.poll();
             
-            for(int node : graph[cur[0]]){
-                if(inDegree[node]>cur[1]+1){
-                    inDegree[node] = cur[1]+1;
-                    max = Math.max(inDegree[node], max);
-                    q.add(new int[]{node,cur[1]+1});
-                }
+            for(int next : graph[cur[0]]){
+                if(visited[next]) continue;
+                visited[next] = true;
+                q.add(new int[]{next,cur[1]+1});
+                dist[cur[1]+1]++;
             }
         }
         
-        for(int e : inDegree){
-            if(e == max) answer++;
+        for(int i = 0; i<=n; i++){
+            if(dist[i]!=0) answer = dist[i];
         }
         
         return answer;
